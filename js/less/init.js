0acc1e5c177603f5cb110780ba00d73262f29e3a
Paul Hachmang <paul@paul.local>
2014-04-28 14:40:02 +0200

    Upgraded to latest version of lessjs

--- js/less/init.js
+++ js/less/init.js
@@ -1,9 +1,10 @@
 less = {
-    env: "development", // or "production"
-    async: true,        // load imports async
-    poll: 2000,         // when in watch mode, time in ms between polls
-    dumpLineNumbers: "all" // or "mediaQuery" or "all"
-//    relativeUrls: false // whether to adjust url's to be relative
-//                        // if false, url's are already relative to the
-//                        // entry less file
+    env: "development",
+       async: false,
+       fileAsync: false,
+       poll: 1000,
+       functions: {},
+       dumpLineNumbers: "all",
+       relativeUrls: false
+//       rootpath: ":/a.com/"
 };
